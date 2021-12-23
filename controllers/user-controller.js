40b82821f036999418a19e078aa509dfe252e136
Dekelbd <dekeldbd@gmail.com>
2021-12-23 15:05:49 +0200

    Create server and user controller

---
+++ controllers/user-controller.js
@@ -0,0 +1,62 @@
+const User = require('../models/user-model');
+const HttpError = require('../utils/HttpError');
+
+exports.getUsers = async (req, res, next) => {
+  try {
+    if (!req.user.isManager) {
+      throw new HttpError('Unauthorized', 401);
+    }
+    const users = await User.find({}, 'fullName _id');
+    if (!users) {
+      throw new HttpError('cant find users', 404);
+    }
+    res.status(200).json({message: 'success', data: users});
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      throw new HttpError('cant find this user', 404);
+    }
+    const populatedUser = await user
+      .populate('posts')
+      .populate('following')
+      .exec();
+    if (!populatedUser) {
+      throw new HttpError('cant get user data', 404);
+    }
+    res.status(200).json({message: 'success', data: populatedUser});
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.deleteUser = async (req, res, next) => {
+  try {
+    if (!req.user.isManager || !req.user._id === req.params.userId) {
+      throw new HttpError('Unauthorized', 401);
+    }
+    await User.deleteOne({_id: req.params.userId});
+    res.status(200).json({message: 'success', data: null});
+  } catch (e) {
+    next(e);
+  }
+};
+
+exports.updateUser = async (req, res, next) => {
+  try {
+    if (!req.user._id === req.params.userId) {
+      throw new HttpError('Unauthorized', 401);
+    }
+    const user = req.user;
+    Object.assign(user, req.body);
+    await user.save();
+    res.status(200).json({message: 'success', data: user});
+  } catch (e) {
+    next(e);
+  }
+};
