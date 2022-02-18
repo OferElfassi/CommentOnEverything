19ed97e09cf1fa0af0603f2363a997e97d8ba050
ofer221 <ofer2212@gmail.com>
2022-02-18 23:59:20 +0200

    updating the new post flow

--- config/keys.js
+++ config/keys.js
@@ -4,5 +4,5 @@ require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
 } else {
-  module.exports = require("./prod");
+  module.exports = require("./dev");
 }
